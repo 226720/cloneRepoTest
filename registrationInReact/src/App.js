93754d6b09459110d8492dd1291c1b128fa44da7
226720 <124857164+226720@users.noreply.github.com>
2023-02-22 18:57:51 +0530

    Add files via upload

---
+++ registrationInReact/src/App.js
@@ -0,0 +1,10 @@
+import "./App.css";
+import Register from "./funct-component/Register";
+function App() {
+  return (
+    <div className="App">
+      <Register />
+    </div>
+  );
+}
+export default App;

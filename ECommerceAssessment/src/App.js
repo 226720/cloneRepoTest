1e2c85e31c14be84c372340df4888df753338938
226720 <124857164+226720@users.noreply.github.com>
2023-02-23 09:02:26 +0530

    Add files via upload

---
+++ ECommerceAssessment/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import ProductInfo from './component/ProductInfo';
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Home Appliances </h1><br/>
+     <ProductInfo />
+    </div>
+  );
+}
+
+export default App;

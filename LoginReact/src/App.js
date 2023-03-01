50337f3902973a3ce0f3227687f2b627c6f64cb4
226720 <124857164+226720@users.noreply.github.com>
2023-03-01 17:49:26 +0530

    Add files via upload

---
+++ LoginReact/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Login from './component/Login';
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Login </h1><br/>
+     <Login />
+    </div>
+  );
+}
+
+export default App;

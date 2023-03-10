96409c62eef37c65137670f5168160fdeaf78c72
226720 <124857164+226720@users.noreply.github.com>
2023-03-10 11:57:45 +0530

    Update UserInfo.js

--- FinalAssessment/src/components/UserInfo.js
+++ FinalAssessment/src/components/UserInfo.js
@@ -1,9 +1,6 @@
 import React,{useState, useEffect} from 'react';
 import MaterialTable from 'material-table';
 
-import React,{useState, useEffect} from 'react';
-import MaterialTable from 'material-table';
-
 function UserInfo() {
 
   const [data, setData] = useState([])
@@ -26,12 +23,43 @@ function UserInfo() {
   return (
     <div className="App">
       <h1 align="center">React-App</h1>
-      <h4 align='center'>Material Table</h4>
+      <h4 align='center'>Material Table with CRUD operation</h4>
       <MaterialTable
         title="Employee Data"
         data={data}
         columns={columns}
-      />
+        editable={{
+          onRowAdd: (newRow) => new Promise((resolve, reject) => {
+            const updatedRows = [...data, { id: Math.floor(Math.random() * 100), ...newRow }]
+            setTimeout(() => {
+              setData(updatedRows)
+              resolve()
+            }, 2000)
+          }),
+          onRowDelete: selectedRow => new Promise((resolve, reject) => {
+            const index = selectedRow.tableData.id;
+            const updatedRows = [...data]
+            updatedRows.splice(index, 1)
+            setTimeout(() => {
+              setData(updatedRows)
+              resolve()
+            }, 2000)
+          }),
+          onRowUpdate:(updatedRow,oldRow)=>new Promise((resolve,reject)=>{
+            const index=oldRow.tableData.id;
+            const updatedRows=[...data]
+            updatedRows[index]=updatedRow
+            setTimeout(() => {
+              setData(updatedRows)
+              resolve()
+            }, 2000)
+          })
+
+        }}
+        options={{
+          actionsColumnIndex: -1, addRowPosition: "first"
+        }}
+        />
     </div>
   );
 }

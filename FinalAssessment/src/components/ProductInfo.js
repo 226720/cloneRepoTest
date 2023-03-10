d539019af73cf44919c5ba630b2e1ebd18fab959
226720 <124857164+226720@users.noreply.github.com>
2023-03-10 13:13:15 +0530

    Update ProductInfo.js

--- FinalAssessment/src/components/ProductInfo.js
+++ FinalAssessment/src/components/ProductInfo.js
@@ -1,67 +1,48 @@
-import React, { Component } from "react";
-import Product from "./Product";
-import {} from "./Product.css";
+import { React, useState, useEffect } from "react";
+import {
+  ImageList,
+  ImageListItem,
+  ImageListItemBar,
+  IconButton,
+} from "@mui/material";
 
-export class ProductInfo extends Component {
-  products = [
-    {
-      title: "IKEA Steel water bottle",
-      type: "Water bottle",
-      description: "water bottle which made of glass",
-      url: "https://cdn.igp.com/f_auto,q_auto,t_pnopt12prodlp/products/p-drink-up-personalized-glass-water-bottle-1000ml-135901-m.jpg",
-      price: "Rs.480",
-      rating: 4.5,
-    },
-    {
-      title: "Copper water bottle",
-      type: "Water bottle",
-      description: "Personalised water bottle made of copper",
-      url: "https://cdn.igp.com/f_auto,q_auto,t_pnopt12prodlp/products/p-drink-all-your-water-personalized-copper-water-bottle-146321-m.jpg",
-      price: "Rs.790",
-      rating: 5,
-    },
-    {
-      title: "NutraBay water bottle",
-      type: "Water bottle",
-      description: "NutraBay Shaker water bottle",
-      url: "https://cdn.nutrabay.com/wp-content/uploads/2022/02/NB-NUT-1021-PA-01x-768x768.jpg",
-      price: "Rs.490",
-      rating: 3.5,
-    },
-    {
-      title: "Personalised Water bottle",
-      type: "water bottle",
-      description: "Stainless steel personalised water bottle for kids",
-      url: "https://cdn.igp.com/f_auto,q_auto,t_pnopt12prodlp/products/p-personalized-water-bottle-159302-m.jpg",
-      price: "Rs.690",
-      rating: 4,
-    },
-  ];
-  constructor() {
-    super();
-    this.state = {
-      products: this.products,
-    };
-  }
-  render() {
-    return (
-      <div align="center">
-        <h2>Products</h2>
-        {this.state.products.map((appliance) => {
-          return (
-            <div key={appliance.price}>
-              <Product
-                title={appliance.title}
-                price={appliance.price}
-                description={appliance.description}
-                imageUrl={appliance.url}
-              />
-            </div>
-          );
-        })}
-      </div>
-    );
-  }
+export function ProductInfo() {
+  const [datas, setDatas] = useState([]);
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/photos")
+      .then((response) => response.json())
+      .then((Data) => {
+        setDatas(Data);
+      });
+  }, []);
+  return (
+    <ImageList
+      sx={{ width: 1000, height: 400 }}
+      cols={4}
+      rowHeight={164}
+      component="main"
+    >
+      {datas.map((item) => (
+        <ImageListItem key={item.id}>
+          <img
+            src={`${item.url}?w=164&h=164&fit=crop&auto=format`}
+            srcSet={`${item.url}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
+            alt={item.url}
+            loading="lazy"
+          />
+
+          <ImageListItemBar
+            title={item.title}
+            actionIcon={
+              <IconButton
+                sx={{ color: "white" }}
+                aria-label={`info about ${item.title}`}
+              ></IconButton>
+            }
+          />
+        </ImageListItem>
+      ))}
+    </ImageList>
+  );
 }
-
 export default ProductInfo;

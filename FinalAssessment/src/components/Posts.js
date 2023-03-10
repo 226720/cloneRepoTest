4c11562a38f49b0cf72395071e616bb43bc5c736
226720 <124857164+226720@users.noreply.github.com>
2023-03-10 11:30:09 +0530

    Add files via upload

---
+++ FinalAssessment/src/components/Posts.js
@@ -0,0 +1,48 @@
+import { React, useState, useEffect } from "react";
+import {
+  ImageList,
+  ImageListItem,
+  ImageListItemBar,
+  IconButton,
+} from "@mui/material";
+
+export function Posts() {
+  const [datas, setDatas] = useState([]);
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/photos")
+      .then((response) => response.json())
+      .then((Data) => {
+        setDatas(Data);
+      });
+  }, []);
+  return (
+    <ImageList
+      sx={{ width: 1000, height: 400 }}
+      cols={4}
+      rowHeight={164}
+      component="main"
+    >
+      {datas.map((item) => (
+        <ImageListItem key={item.id}>
+          <img
+            src={`${item.url}?w=164&h=164&fit=crop&auto=format`}
+            srcSet={`${item.url}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
+            alt={item.url}
+            loading="lazy"
+          />
+
+          <ImageListItemBar
+            title={item.title}
+            actionIcon={
+              <IconButton
+                sx={{ color: "white" }}
+                aria-label={`info about ${item.title}`}
+              ></IconButton>
+            }
+          />
+        </ImageListItem>
+      ))}
+    </ImageList>
+  );
+}
+export default Posts;

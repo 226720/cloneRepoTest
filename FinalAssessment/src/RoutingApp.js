58715c50e3ad1a2517b628aef06fac1ff9cad043
226720 <124857164+226720@users.noreply.github.com>
2023-03-10 11:29:29 +0530

    Update RoutingApp.js

--- FinalAssessment/src/RoutingApp.js
+++ FinalAssessment/src/RoutingApp.js
@@ -7,6 +7,7 @@ import {createBrowserRouter,RouterProvider} from 'react-router-dom';
 import Password from "./components/Password";
 import ProductInfo from "./components/ProductInfo";
 import UserInfo from "./components/UserInfo";
+import Posts from "./components/Posts";
 
 const router= createBrowserRouter([
     {path:'/',element:<Login/> },
@@ -14,9 +15,11 @@ const router= createBrowserRouter([
     {path:'/signup', element:<Signup/>},
     {path:'/dashboard', element:<Dashboard />},
     {path:'/products', element:<ProductInfo />},
-    {path:'/users', element:<UserInfo />}
+    {path:'/users', element:<UserInfo />},
+    {path:'/posts', element:<Posts/>}
 ]);
 function App(){
     return <RouterProvider router={router}/>
 };
 export default App;
+
